8652ea746922d7a9abd6a66634db6c1e5a348b70
Fabio <fa.espinosa10@uniandes.edu.co>
2019-02-15 12:56:52 +0100

    add function

--- views/run.sql
+++ views/run.sql
@@ -8,26 +8,29 @@ BEGIN;
 
 
     CREATE OR REPLACE FUNCTION
-    merge(jsonb, jsonb) RETURNS jsonb
+    merge_jsonb
+    (jsonb, jsonb) RETURNS jsonb
 AS 'SELECT $1 || $2;'
 LANGUAGE SQL                                   
     IMMUTABLE
     RETURNS NULL ON NULL INPUT;
 
-CREATE AGGREGATE oms_attributes(jsonb)
-(sfunc =
-merge, stype = jsonb, initcond = '{}');
+    CREATE AGGREGATE oms_attributes(jsonb)
+    (sfunc =
+merge_jsonb, stype = jsonb, initcond = '{}');
 
 CREATE AGGREGATE rr_attributes(jsonb)
 (sfunc =
-merge, stype = jsonb, initcond = '{}');
+merge_jsonb, stype = jsonb, initcond = '{}');
 
 
-CREATE OR REPLACE VIEW "RunView" as 
-SELECT "RunEvent"."run_number", 
-oms_attributes("RunEvent"."oms_metadata" 
-ORDER BY "RunEvent"."version" ), 
-rr_attributes("RunEvent"."rr_metadata" 
+CREATE OR REPLACE VIEW "RunView" as
+SELECT "RunEvent"."run_number",
+    oms_attributes("RunEvent"."oms_metadata"
+ORDER BY "RunEvent"."version"
+), 
+rr_attributes
+("RunEvent"."rr_metadata" 
 ORDER BY "RunEvent"."version")
 FROM "RunEvent" INNER JOIN "Event" ON "Event"."version" = "RunEvent"."version"
 GROUP BY "RunEvent"."run_number";
