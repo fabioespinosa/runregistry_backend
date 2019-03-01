da68fe1aeaa049a82ce77b86283440fc271b8be6
Fabio <fa.espinosa10@uniandes.edu.co>
2019-03-01 12:23:51 +0100

    add transaction to JSONB deduplication

--- controllers/JSONBDeduplication.js
+++ controllers/JSONBDeduplication.js
@@ -1,16 +1,26 @@
 const JSONBDeduplication = require('../models').JSONBDeduplication;
 
 // It will try to find one JSONB already in the JSONBDeduplication table. If it finds it, it will return it, if it doesn't find it, it will create it.
-exports.findOrCreateJSONB = async jsonb => {
-    let saved_jsonb = await JSONBDeduplication.findOne({
-        where: {
-            jsonb
-        }
-    });
+exports.findOrCreateJSONB = async (jsonb, transaction) => {
+    const options = {};
+    if (transaction) {
+        options.transaction = transaction;
+    }
+    let saved_jsonb = await JSONBDeduplication.findOne(
+        {
+            where: {
+                jsonb
+            }
+        },
+        options
+    );
     if (saved_jsonb === null) {
-        saved_jsonb = await JSONBDeduplication.create({
-            jsonb
-        });
+        saved_jsonb = await JSONBDeduplication.create(
+            {
+                jsonb
+            },
+            options
+        );
     }
     return saved_jsonb.dataValues;
 };
