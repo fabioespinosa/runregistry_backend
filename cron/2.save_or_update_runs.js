0f509ba97062851d96559864b2cfa6897b016c61
Fabio <fa.espinosa10@uniandes.edu.co>
2019-07-17 18:51:16 +0200

    fix refreshing of runs

--- cron/2.save_or_update_runs.js
+++ cron/2.save_or_update_runs.js
@@ -126,112 +126,127 @@ exports.update_runs = async (
     number_of_tries = 0,
     { email, comment, manually_significant, previous_rr_attributes }
 ) => {
-    let updated_runs = 0;
-    const runs_not_updated = [];
-    const promises = runs_to_update.map(run => async () => {
-        // We only update a run which state is OPEN
-        try {
-            // We get the lumisections from OMS:
-            const oms_lumisections = await get_OMS_lumisections(run.run_number);
-            const oms_attributes = await calculate_oms_attributes(
-                run,
-                oms_lumisections
-            );
-            // We freeze oms_attributes to prevent them changing later on:
-            Object.freeze(oms_lumisections);
-            Object.freeze(oms_attributes);
-            const rr_attributes = await calculate_rr_attributes(
-                oms_attributes,
-                oms_lumisections,
-                previous_rr_attributes // If it was manually updated (see method below), this will not be undefined
-            );
-            let rr_lumisections = [];
-            // Only if the run is significant, do we calculate the component statuses for the run
-            if (rr_attributes.significant || manually_significant) {
-                rr_lumisections = await calculate_rr_lumisections(
-                    oms_attributes,
-                    rr_attributes,
+    return new Promise(async (resolve, reject) => {
+        let updated_runs = 0;
+        const runs_not_updated = [];
+        const promises = runs_to_update.map(run => async () => {
+            // We only update a run which state is OPEN
+            try {
+                // We get the lumisections from OMS:
+                const oms_lumisections = await get_OMS_lumisections(
+                    run.run_number
+                );
+                const oms_attributes = await calculate_oms_attributes(
+                    run,
                     oms_lumisections
                 );
-            }
-            const updated_run = await axios.put(
-                `${API_URL}/automatic_run_update/${run.run_number}`,
-                {
+                // We freeze oms_attributes to prevent them changing later on:
+                Object.freeze(oms_lumisections);
+                Object.freeze(oms_attributes);
+                const rr_attributes = await calculate_rr_attributes(
                     oms_attributes,
                     oms_lumisections,
-                    rr_attributes,
-                    rr_lumisections
-                },
-                {
-                    // The email HAS to start with auto, or else API won't know it's an automatic change (unless it was manually requested to update)
-                    headers: {
-                        email: `auto@auto${email && ` - ${email}`}`,
-                        comment: comment || 'automatic update from OMS',
-                        // Avoid permission egroups problem:
-                        egroups: 'cms-dqm-runregistry-experts;'
-                    },
-                    maxContentLength: 52428890
+                    previous_rr_attributes // If it was manually updated (see method below), this will not be undefined
+                );
+                let rr_lumisections = [];
+                // Only if the run is significant, do we calculate the component statuses for the run
+                if (rr_attributes.significant || manually_significant) {
+                    rr_lumisections = await calculate_rr_lumisections(
+                        oms_attributes,
+                        rr_attributes,
+                        oms_lumisections
+                    );
                 }
-            );
-            if (updated_run.status === 200) {
-                // if status is 200 the run was actually updated, if status is 204, the request was processed but there was nothing to update
-                updated_runs += 1;
+                const updated_run = await axios.put(
+                    `${API_URL}/automatic_run_update/${run.run_number}`,
+                    {
+                        oms_attributes,
+                        oms_lumisections,
+                        rr_attributes,
+                        rr_lumisections
+                    },
+                    {
+                        // The email HAS to start with auto, or else API won't know it's an automatic change (unless it was manually requested to update)
+                        headers: {
+                            email: `auto@auto${email && ` - ${email}`}`,
+                            comment: comment || 'automatic update from OMS',
+                            // Avoid permission egroups problem:
+                            egroups: 'cms-dqm-runregistry-experts;'
+                        },
+                        maxContentLength: 52428890
+                    }
+                );
+                if (updated_run.status === 200) {
+                    // if status is 200 the run was actually updated, if status is 204, the request was processed but there was nothing to update
+                    updated_runs += 1;
+                }
+            } catch (e) {
+                console.log(`Error updating run ${run.run_number}`);
             }
-        } catch (e) {
-            console.log(`Error updating run ${run.run_number}`);
+        });
+        if (runs_to_update.length < 10) {
+            // If it is less than 10 runs we are refreshing, no need to do a queue
+            await Promise.all(promises.map(async promise => await promise()));
+            resolve();
+        } else {
+            // We use 3 workers to save it in first try, if errors, then we want to go slower, just 1:
+            const number_of_workers = 1;
+            const asyncQueue = queue(
+                async run => await run(),
+                number_of_workers
+            );
+
+            // When runs finished updating:
+            asyncQueue.drain = async () => {
+                console.log(`${updated_runs} run(s) updated`);
+                if (runs_not_updated.length > 0) {
+                    const run_numbers_of_runs_not_updated = runs_not_updated.map(
+                        ({ run_number }) => run_number
+                    );
+                    console.log(
+                        `WARNING: ${
+                            runs_not_updated.length
+                        } run(s) were not updated. They are: ${run_numbers_of_runs_not_updated}.`
+                    );
+                    console.log('------------------------------');
+                    console.log('------------------------------');
+                    if (number_of_tries < 4) {
+                        console.log(
+                            `TRYING AGAIN: with ${
+                                runs_not_updated.length
+                            } run(s)`
+                        );
+                        number_of_tries += 1;
+                        await exports.update_runs(
+                            runs_not_updated,
+                            number_of_tries,
+                            {
+                                email,
+                                comment,
+                                manually_significant,
+                                previous_rr_attributes
+                            }
+                        );
+                        resolve();
+                    } else {
+                        console.log(
+                            `After trying 4 times, ${run_numbers_of_runs_not_updated} run(s) were not updated`
+                        );
+                        reject();
+                    }
+                }
+                resolve();
+            };
+
+            asyncQueue.error = err => {
+                console.log(
+                    `Critical error saving runs, ${JSON.stringify(err)}`
+                );
+            };
+
+            asyncQueue.push(promises);
         }
     });
-    if (runs_to_update.length < 10) {
-        // If it is less than 10 runs we are refreshing, no need to do a queue
-        await Promise.all(promises.map(async promise => await promise()));
-    } else {
-        // We use 3 workers to save it in first try, if errors, then we want to go slower, just 1:
-        const number_of_workers = 1;
-        const asyncQueue = queue(async run => await run(), number_of_workers);
-
-        // When runs finished updating:
-        asyncQueue.drain = async () => {
-            console.log(`${updated_runs} run(s) updated`);
-            if (runs_not_updated.length > 0) {
-                const run_numbers_of_runs_not_updated = runs_not_updated.map(
-                    ({ run_number }) => run_number
-                );
-                console.log(
-                    `WARNING: ${
-                        runs_not_updated.length
-                    } run(s) were not updated. They are: ${run_numbers_of_runs_not_updated}.`
-                );
-                console.log('------------------------------');
-                console.log('------------------------------');
-                if (number_of_tries < 4) {
-                    console.log(
-                        `TRYING AGAIN: with ${runs_not_updated.length} run(s)`
-                    );
-                    number_of_tries += 1;
-                    await exports.update_runs(
-                        runs_not_updated,
-                        number_of_tries,
-                        {
-                            email,
-                            comment,
-                            manually_significant,
-                            previous_rr_attributes
-                        }
-                    );
-                } else {
-                    console.log(
-                        `After trying 4 times, ${run_numbers_of_runs_not_updated} run(s) were not updated`
-                    );
-                }
-            }
-        };
-
-        asyncQueue.error = err => {
-            console.log(`Critical error saving runs, ${JSON.stringify(err)}`);
-        };
-
-        asyncQueue.push(promises);
-    }
 };
 
 exports.manually_update_a_run = async (
